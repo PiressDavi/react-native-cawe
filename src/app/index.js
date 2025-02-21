7f58eaa4248e53e7bb27636506ac36079fdc6496
Davi Pires de Souza <110909951+PiressDavi@users.noreply.github.com>
2025-02-21 19:35:01 +0000

    [feat] expo-router

--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Minha Primeira Aplicação</Text>
+      <Text>Aplicativo Pronto para Usar</Text>
       <StatusBar style="auto" />
     </View>
   );
